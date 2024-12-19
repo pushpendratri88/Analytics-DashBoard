3f0f54846298ba10d0d1defcf62b8b7e9d4ad7a1
Pushpendra.tripathi <pushpendra.tri88@gmail.com>
2024-12-20 01:21:47 +0530

    1. code refactoring

--- src/main/java/com/brainstorm/analyticsdashboard/config/ElasticsearchConfig.java
+++
@@ -1,8 +0,0 @@
-//package com.brainstorm.analyticsdashboard.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class ElasticsearchConfig {
-//
-//}
